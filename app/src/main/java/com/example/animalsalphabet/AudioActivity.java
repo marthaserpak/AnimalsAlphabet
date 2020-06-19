0663dd6562fdab2fca10f9b01b33e0c91f404db6
marthaserpak <m.v.serpak@gmail.com>
2020-06-19 12:07:58 +0300

    new version

--- app/src/main/java/com/example/animalsalphabet/AudioActivity.java
+++ app/src/main/java/com/example/animalsalphabet/AudioActivity.java
@@ -2,10 +2,13 @@ package com.example.animalsalphabet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.media.Image;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import java.lang.reflect.Array;
@@ -13,18 +16,25 @@ import java.lang.reflect.Array;
 public class AudioActivity extends AppCompatActivity {
 
     MediaPlayer lettersPronunciation;
+    Button btn_share;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_audio);
-
-        lettersPronunciation = MediaPlayer.create(this, R.raw.);
-
-        }
-
-    public void letterA(View view) {
-
+        btn_share = findViewById(R.id.btn_share);
+        lettersPronunciation = MediaPlayer.create(getApplicationContext(), R.raw.alphabet_song);
     }
 
+    public void btn_audio(View view) {
+        lettersPronunciation.isPlaying();
+    }
+
+    public void shareApp(View view) {
+        String message = "Try this app for kids!";
+        Uri destination = Uri.parse("smsto:5554");
+        Intent smsIntent = new Intent(Intent.ACTION_SENDTO, destination);
+        smsIntent.putExtra("sms_body", message);
+        startActivity(smsIntent);
+    }
 }
